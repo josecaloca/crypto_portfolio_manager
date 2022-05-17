3a82a834b58bdae0644de397b7382226ac0f4b39
j-33-t <prasenjeet.rathore@gmail.com>
2022-05-17 23:24:55 +0200

    main page contribute adding

--- app.py
+++ app.py
@@ -44,3 +44,10 @@ st.markdown('''Jiyoung Kim''')
     [![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/jykcr/)
 '''
 st.markdown("<br>",unsafe_allow_html=True)
+
+st.markdown('''Prasenjeet Rathore''')
+'''
+    [![Github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/j-33-t) 
+    [![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/prasenjeet-j33t/)
+'''
+st.markdown("<br>",unsafe_allow_html=True)
