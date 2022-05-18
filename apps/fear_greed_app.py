b23d002349c56feaca1725832cb971e3c43a98f6
j-33-t <prasenjeet.rathore@gmail.com>
2022-05-18 14:00:07 +0200

    circle adjustment - 6

--- apps/fear_greed_app.py
+++ apps/fear_greed_app.py
@@ -64,8 +64,8 @@ def app():
             shapes=[
                 go.layout.Shape(
                     type="circle",
-                    x0=0.48, x1=0.51,
-                    y0=0.48, y1=0.51,
+                    x0=0.49, x1=0.51,
+                    y0=0.49, y1=0.51,
                     fillcolor="#333",
                     line_color="#333",
                 ),
